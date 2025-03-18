793dd894c8dbd592498d102819f3d498ba7df7c3
SerityAi <156796021+SerityAi@users.noreply.github.com>
2025-03-18 20:16:30 +0700

    Update Loader.lua

--- Loader/main/Loader.lua
+++ Loader/main/Loader.lua
@@ -1,3 +1,4 @@
+repeat wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 if game.PlaceId == 16732694052 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/SerityAi/Zenon-Trash/refs/heads/main/Fisch.lua"))()
 end
