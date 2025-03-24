0ea7b7e63e187ac25a2da5ba9cbfef924bac7bed
SunZens <156796021+SunZens@users.noreply.github.com>
2025-03-24 22:41:41 +0700

    Update Loader.lua

--- Loader/main/Loader.lua
+++ Loader/main/Loader.lua
@@ -1,4 +1,4 @@
 repeat wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
-if game.PlaceId == 16732694052 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/SerityAi/Zenon-Trash/refs/heads/main/Fisch.lua"))()
+if game.PlaceId == 13772394625 or game.PlaceId == 15234596844 or game.PlaceId == 14915220621 or game.PlaceId == 15264892126 or game.PlaceId == 15517169103 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/SunZens/Zenon-Trash/refs/heads/main/BladeBall.lua"))()
 end
