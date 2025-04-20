bfe7c2dc498567a9281ca338f6330844e3e79c36
SunZens <156796021+SunZens@users.noreply.github.com>
2025-04-20 19:27:08 +0700

    Create FuckYou.lua

---
+++ FuckYou.lua
@@ -0,0 +1,9 @@
+if game:GetService("Players").LocalPlayer.Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+    if not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Jerk Off") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Jerk Off") then
+        loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))()
+    end
+else
+    if not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Jerk Off R15") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Jerk Off R15") then
+        loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
+    end
+end
