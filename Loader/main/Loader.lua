4faa4dfae1a0fa9dc2889ff7a9c583b83c2ed7c7
SunZens <156796021+SunZens@users.noreply.github.com>
2025-04-23 16:10:43 +0700

    Update Loader.lua

--- Loader/main/Loader.lua
+++ Loader/main/Loader.lua
@@ -1,4 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 if game.PlaceId == 13772394625 or game.PlaceId == 15234596844 or game.PlaceId == 14915220621 or game.PlaceId == 15264892126 or game.PlaceId == 15517169103 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/SunZens/Zenon-Trash/refs/heads/main/BladeBall.lua"))()
+elseif game.PlaceId == 126884695634066 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/SunZens/Zenon-Trash/refs/heads/main/Grow%20A%20Garden.lua"))()
 end
